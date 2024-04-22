bcb15d11c6661240a218ea283dead72b5667bdc3
JhoelHairton <jhoelhairton770@gmail.com>
2024-04-22 02:04:37 -0500

    users

---
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -0,0 +1,19 @@
+package com.example.userservice;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
+
+
